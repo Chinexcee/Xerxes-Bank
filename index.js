cb482645d95e1af20c5b28b704c6928cabeff9d5
chinedu mnbachu <chinexcee@gmail.com>
2024-11-28 09:58:28 +0100

    The landing page for my Xerxes Bank

---
+++ index.js
@@ -0,0 +1,6 @@
+// Optional Chaining (?.) Used to access deeply nested properties without worrying about undefined errors
+// let user = {name: 'chinedu'};
+// console.log(user?.name?.);
+
+// Nullish Coalescing(??) provides a fallback only when the value is null or undefined
+
